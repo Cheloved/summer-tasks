f5861893ffd13fe84b29248995d5e67f200b5149
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-01 01:41:04 +0300

    (task-2) Заготовка задачи

---
+++ task-2/client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" [i] Client started\n");
+    return 0;
+}
---
+++ task-2/server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" [i] Server started\n");
+    return 0;
+}
