c4f5e0c469d2ec6260f4d27d1657863761007882
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-04 02:50:57 +0300

    (task-4) Парсинг коротких опций

--- task-4/long.c
+++ task-4/long.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <getopt.h>
 
 int main(int argc, char** argv)
 {
-    printf("test\n");
 
     return 0;
 }
---
+++ task-4/short.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    printf("Correct short options: ");
+    int opt;
+    while ( (opt = getopt(argc, argv, "mcst")) != -1 )
+    {
+        switch ( opt )
+        {
+            case 'm': printf("m "); break;
+            case 'c': printf("c "); break;
+            case 's': printf("s "); break;
+            case 't': printf("t "); break;
+        }
+    }
+
+    printf("\nNon-option arguments: ");
+    while ( optind < argc )
+        printf("%s ", argv[optind++]);
+
+    return 0;
+}
