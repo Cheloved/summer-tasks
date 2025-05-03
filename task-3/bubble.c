d5245d8e1de0668504920dd1fd474b4e3ace04ce
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-03 20:55:16 +0300

    (task-3) Сортировка пузырьком

---
+++ task-3/bubble.c
@@ -0,0 +1,22 @@
+#include "bubble.h"
+
+int bubble_sort(char* input, long* array, long length, long* result)
+{
+    for ( int i = 0; i < length; i++ )
+        result[i] = array[i];
+
+    for ( int i = 0; i < length; i++ )
+    {
+        for ( int j = 0; j < length - i - 1; j++ )
+        {
+            if ( !linecmp(input, result[j], result[j+1]) ) 
+            {
+                int buf = result[j];
+                result[j] = result[j+1];
+                result[j+1] = buf;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ task-3/bubble.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "utils.h"
+
+int bubble_sort(char* input, long* array, long length, long* result);
