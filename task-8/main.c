cb2e2f8cbf397fb5b5b00ca91d6f1871a0eb3fd3
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-05 01:58:36 +0300

    (task-8) Готовый функционал в main

--- task-8/main.c
+++ task-8/main.c
@@ -7,6 +7,8 @@
 #include "reading.h"
 #include "matrix.h"
 
+const int SIZE = 1000;
+
 void handle_sigint_signal(int sig)
 {
     printf(" Row in a: %d, col in b: %d\n", global_row, global_col); 
@@ -30,6 +32,7 @@ int main(int argc, char** argv)
     if ( argc != 2 )
     {
         fprintf(stderr, "Укажите способ изменения реакции(signal/sigaction)\n");
+        fprintf(stderr, "или generate для генерации матриц a.txt и b.txt");
         return -1;
     }
 
@@ -43,28 +46,31 @@ int main(int argc, char** argv)
         sigemptyset(&sa.sa_mask);
         sa.sa_flags = 0;
         sigaction(SIGINT, &sa, NULL);
+    } else if ( !strcmp(argv[1], "generate") ) {
+        generate_matrix("a.txt", SIZE, -10, 10);
+        generate_matrix("b.txt", SIZE, -10, 10);
     } else {
-        fprintf(stderr, "Недоступный способ!\n");
-        fprintf(stderr, "Возможные варианты: signal, sigaction\n");
+        fprintf(stderr, "Неверный аргумент!\n");
+        fprintf(stderr, "Возможные варианты: signal, sigaction, generate\n");
         return -2;
     }
 
-    int** mat_a = init_mat_square(4);
-    int** mat_b = init_mat_square(4);
-    int** result = init_mat_square(4);
+    int** mat_a = init_mat_square(SIZE);
+    int** mat_b = init_mat_square(SIZE);
+    int** result = init_mat_square(SIZE);
 
-    read_matrix("a.txt", mat_a, 1000, 1000, 16384);
-    read_matrix("b.txt", mat_b, 1000, 1000, 16384);
+    read_matrix("a.txt", mat_a, SIZE, SIZE, 16384);
+    read_matrix("b.txt", mat_b, SIZE, SIZE, 16384);
 
-    print_mat(mat_a, 4);
-    print_mat(mat_b, 4);
-    matrix_multiply_square(4, mat_a, mat_b, result);
+    // print_mat(mat_a, 4);
+    // print_mat(mat_b, 4);
+    matrix_multiply_square(SIZE, mat_a, mat_b, result);
 
-    print_mat(result, 4);
+    // print_mat(result, 4);
 
-    free_mat_square(mat_a, 4);
-    free_mat_square(mat_b, 4);
-    free_mat_square(result, 4);
+    free_mat_square(mat_a, SIZE);
+    free_mat_square(mat_b, SIZE);
+    free_mat_square(result, SIZE);
 
     return 0;
 }
