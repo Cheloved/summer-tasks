1e6979d5ed6e38f57289fdf48cd1c3778c89ded3
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-04 20:06:38 +0300

    (task-5) Основные способы вывода ошибки

--- task-5/main.c
+++ task-5/main.c
@@ -3,9 +3,28 @@
 #include <errno.h>
 #include <string.h>
 
+// extern const char* sys_errlist[];
+
 int main()
 {
-    printf("test\n");
+    // Попытка открыть несуществующий файл
+    FILE* file = fopen("not_exist.txt", "r");
 
+    // Вывод ошибки через perror()
+    if ( !file )
+        perror("Ошибка из perror()");
+
+    // Вывод ошибки через strerror()
+    if ( !file )
+        fprintf(stderr, "Ошибка из strerror(): %s\n", strerror(errno));
+
+    // Вывод только errno
+    if ( !file )
+        fprintf(stderr, "Ошибка. errno: %d\n", errno);
+
+    // Вывод ошибки через sys_errlist[errno]
+    // if ( !file )
+    //     fprintf(stderr, "Ошибка из sys_errlist[errno]: %s\n", sys_errlist[errno]);
+        
     return 0;
 }
