a2d3f13dcdf0cb8451c6b4314c4b171bfa72d0de
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-05 01:15:25 +0300

    (task-8) Чтение в main.c

--- task-8/main.c
+++ task-8/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "reading.h"
+#include "matrix.h"
 
 int main()
 {
-    printf("test\n");
+    int** mat_a = init_mat_square(4);
+    int** mat_b = init_mat_square(4);
+    int** result = init_mat_square(4);
+
+    read_matrix("a.txt", mat_a, 1000, 1000, 16384);
+    read_matrix("b.txt", mat_b, 1000, 1000, 16384);
+
+    print_mat(mat_a, 4);
+    print_mat(mat_b, 4);
+    matrix_multiply_square(4, mat_a, mat_b, result);
+
     return 0;
 }
