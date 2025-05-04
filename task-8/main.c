66ecd3c19f58d1f4430d280e8123b4203eaad6ed
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-05 01:40:54 +0300

    (task-8) Изменение реакции через sigaction

--- task-8/main.c
+++ task-8/main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "reading.h"
 #include "matrix.h"
 
-int main()
+void handle_sigint_signal(int sig)
 {
+    printf(" Row in a: %d, col in b: %d\n", global_row, global_col); 
+
+    signal(SIGINT, SIG_DFL);
+}
+
+void handle_sigint_sigaction(int sig)
+{
+    printf(" Row in a: %d, col in b: %d\n", global_row, global_col); 
+
+    struct sigaction sa;
+    sa.sa_handler = SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+}
+
+int main(int argc, char** argv)
+{
+    if ( argc != 2 )
+    {
+        fprintf(stderr, "Укажите способ изменения реакции(signal/sigaction)\n");
+        return -1;
+    }
+
+    if ( !strcmp(argv[1], "signal") )
+    {
+        signal(SIGINT, handle_sigint_signal);
+    } else if ( !strcmp(argv[1], "sigaction") )
+    {
+        struct sigaction sa;
+        sa.sa_handler = handle_sigint_sigaction;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        sigaction(SIGINT, &sa, NULL);
+    } else {
+        fprintf(stderr, "Недоступный способ!\n");
+        fprintf(stderr, "Возможные варианты: signal, sigaction\n");
+        return -2;
+    }
+
     int** mat_a = init_mat_square(4);
     int** mat_b = init_mat_square(4);
     int** result = init_mat_square(4);
