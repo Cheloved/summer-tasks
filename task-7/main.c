f0151a1357c889c1ef2199923cb66a4370cd2942
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-04 22:25:21 +0300

    (task-7) Чтение и запись в fork()

--- task-7/main.c
+++ task-7/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define CHUNK_SIZE 1024
 
@@ -18,6 +20,22 @@ long get_file_size(FILE* file)
     return size; 
 }
 
+int copy_from_to(FILE* from, long file_size, FILE* to)
+{
+    char chunk[CHUNK_SIZE]; 
+    int chunks_read = 0;
+    long left;
+    while ( (left = file_size - chunks_read * CHUNK_SIZE) > 0 )
+    {
+        long to_read = (left / CHUNK_SIZE > 0) ? CHUNK_SIZE : left;
+        fread(chunk, sizeof(char), to_read, from);
+        fwrite(chunk, sizeof(char), to_read, to);
+        chunks_read++;
+    }
+
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
     if ( argc != 2 )
@@ -26,31 +44,46 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    FILE* file = fopen(argv[1], "rb");
+    FILE* file = fopen(argv[1], "r");
     if ( !file )
     {
         perror(" [E] Ошибка при чтении файла");
         return -1;
     }
 
-    FILE* out = fopen("out.txt", "wb");
+    long file_size = get_file_size(file);
+
+    pid_t pid = fork();
+    if ( pid < 0 )
+    {
+        fprintf(stderr, " [E] Ошибка при fork()\n");
+        return -1;
+    }
+
+    FILE* out;
+    if ( pid == 0 )
+        out = fopen("child_out.txt", "w+");
+    else
+        out = fopen("parent_out.txt", "w+");
+
     if ( !out )
     {
         perror(" [E] Ошибка при открытии файла записи");
         return -1;
     }
 
-    // Считывание файла блоками
-    char chunk[CHUNK_SIZE]; 
-    long file_size = get_file_size(file);
-    int chunks_read = 0;
-    long left;
-    while ( (left = file_size - chunks_read * CHUNK_SIZE) > 0 )
+    copy_from_to(file, file_size, out);
+    fseek(out, 0, SEEK_SET);
+
+    if ( pid == 0 )
     {
-        long to_read = (left / CHUNK_SIZE > 0) ? CHUNK_SIZE : left;
-        fread(chunk, sizeof(char), to_read, file);
-        fwrite(chunk, sizeof(char), to_read, out);
-        chunks_read++;
+        printf("Содержимое файла дочернего процесса: \n");
+        copy_from_to(out, file_size, stdout);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("\nСодержимое файла родительского процесса: \n");
+        copy_from_to(out, file_size, stdout);
     }
 
     fclose(file);
