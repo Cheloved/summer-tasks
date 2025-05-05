69752e2f45f30a734a7374db98892f9bd2649fab
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-06 01:20:33 +0300

    (task-10) Функционал для работы со стеком

---
+++ task-10/stack.c
@@ -0,0 +1,47 @@
+#include "stack.h"
+
+int is_empty(s_stack stack)
+{
+    return stack.top == -1;
+}
+
+int is_full(s_stack stack)
+{
+    return stack.top == stack.max_num - 1;
+}
+
+int push(s_stack stack, char* value)
+{
+    if ( is_full(stack) )
+    {
+        fprintf(stderr, " [E] Переполнение стека\n");
+        return -1;
+    }
+
+    strcpy(stack.elems[++stack.top], value);
+    return 0;
+}
+
+int pop(s_stack stack, char* result)
+{
+    if ( is_empty(stack) )
+    {
+        fprintf(stderr, " [E] Стек пустой, невозможно извлечь элемент\n");
+        return -1;
+    }
+
+    strcpy(result, stack.elems[stack.top--]);
+    return 0;
+}
+
+int peek(s_stack stack, char* result)
+{
+    if ( is_empty(stack) )
+    {
+        fprintf(stderr, " [E] Стек пустой, невозможно извлечь элемент\n");
+        return -1;
+    }
+
+    strcpy(result, stack.elems[stack.top]);
+    return 0;
+}
---
+++ task-10/stack.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    char** elems;
+    int top;
+    int max_num;
+} s_stack;
+
+int is_empty(s_stack stack);
+
+int is_full(s_stack stack);
+
+int push(s_stack stack, char* value);
+
+int pop(s_stack stack, char* result);
+
+int peek(s_stack stack, char* result);
