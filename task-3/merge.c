79f2db2e814e45c32b940047108bf0a4c9927116
Rakitin Ilia <cheloved.0@gmail.com>
2025-05-06 17:22:42 +0300

    (task-3) Рефактор merge.c

--- task-3/merge.c
+++ task-3/merge.c
@@ -1,99 +1,89 @@
 #include "merge.h"
 
+void merge_arrays(s_data* data, long* l, int lsize, long* r, int rsize)
+{
+    int32_t left = 0;  // Индекс в массиве результатов левой части
+    int32_t right = 0; // Аналогично в провой
+    int32_t i = 0;     // Индекс в текущем массиве результатов
+
+    // Объединение массивов с сортировкой,
+    // пока один из них не закончится
+    while ( left < lsize && right < rsize - 1 )
+    {
+        if ( linecmp(data->input, l[left], r[right]) <= 0 ) 
+            data->result[i++] = l[left++];
+        else
+            data->result[i++] = r[right++];
+    }
+
+    // Добавление остаточной части
+    while ( left < lsize )
+        data->result[i++] = l[left++];
+
+    while ( right < rsize - 1 )
+        data->result[i++] = r[right++];
+}
+
 void* merge_sort_thread(void* arg)
 {
     s_data* data = (s_data*)arg; // Структура входных данных
 
     // Длина отрезка массива для данного процесса
     long length = data->right_idx - data->left_idx + 1;
-    if ( length > 1 )
+
+    if ( length == 0 )
+        return NULL;
+
+    // Если остался только один элемент в срезе, 
+    // он и устанавливается в результат
+    if ( length == 1 )
     {
-        // Вычисление середины для дальнейшего разбиения
-        long mid_idx = data->left_idx + (length-1) / 2;
-
-        if ( DEBUG )
-            printf(" [debug] Iter: %d, left: %ld, right: %ld, mid: %ld -> (%ld, %ld) and (%ld, %ld)\n", data->iter, 
-                    data->left_idx,
-                    data->right_idx,
-                    mid_idx,
-                    data->left_idx,
-                    mid_idx,
-                    mid_idx + 1,
-                    data->right_idx);
-
-        // Запуск новых потоков для левой и правой частей
-        // Один из них будет от текущего левого края до середины,
-        // а другой от середины+1 до правого края
-        pthread_t left_thread, right_thread;
-        
-        // Выделение памяти для результата дочернего левого потока
-        long result_left_size = mid_idx - data->left_idx + 1;
-        long* result_left = (long*)(malloc(sizeof(long) * result_left_size));
-        // Сбор остальных данных в структуру
-        s_data data_left = { .iter = data->iter + 1,
-                             .input = data->input,
-                             .ar = data->ar,
-                             .left_idx = data->left_idx,
-                             .right_idx = mid_idx,
-                             .result = result_left};
-
-        // Аналогично для правой части
-        long result_right_size = data->right_idx - mid_idx + 1;
-        long* result_right = (long*)(malloc(sizeof(long) * result_right_size));
-        s_data data_right = { .iter = data->iter + 1,
-                             .input = data->input,
-                             .ar = data->ar,
-                             .left_idx = mid_idx + 1,
-                             .right_idx = data->right_idx,
-                             .result = result_right};
-
-        // Запуск и ожидание завершения
-        pthread_create(&left_thread, NULL, merge_sort_thread, &data_left);
-        pthread_create(&right_thread, NULL, merge_sort_thread, &data_right);
-        pthread_join(left_thread, NULL);
-        pthread_join(right_thread, NULL);
-
-        // === Соединение результатов дочерних потоков === //
-        long left = 0;  // Индекс в массиве результатов левой части
-        long right = 0; // Аналогично в провой
-        long i = 0;     // Индекс в текущем массиве результатов
-
-        // Объединение массивов, пока один из них не закончится
-        while ( left + data->left_idx <= mid_idx && mid_idx + 1 + right <= data->right_idx)
-        {
-            // if ( result_left[left] < result_right[right] ) 
-            // Сравнение строк вместо чисел //
-            if ( linecmp(data->input, result_left[left], result_right[right]) <= 0 ) 
-                data->result[i++] = result_left[left++];
-            else
-                data->result[i++] = result_right[right++];
-        }
-
-        // Добавление остаточной части
-        while ( left + data->left_idx <= mid_idx )
-            data->result[i++] = result_left[left++];
-
-        while ( mid_idx + 1 + right <= data->right_idx )
-            data->result[i++] = result_right[right++];
-
-        if ( DEBUG )
-        {
-            printf(" [debug] Iter: %d, result: ", data->iter);
-            for ( long i = data->left_idx; i <= data->right_idx; i++ )
-                printf("%ld ", data->result[i - data->left_idx]);
-            printf("\n");
-        }
-
-        // Освобождение результатов дочерних потоков из памяти
-        free(result_left);
-        free(result_right);
-    } else {
-        // Если остался только один элемент в срезе, он и устанавливается в результат
         data->result[0] = data->ar[data->left_idx];
-
-        if ( DEBUG )
-            printf(" [debug] LAST! Iter: %d, left: %ld, right: %ld\n", data->iter, data->left_idx, data->right_idx);
+        return NULL;
     }
+
+    // Вычисление середины для дальнейшего разбиения
+    long mid_idx = data->left_idx + (length-1) / 2;
+
+    // Запуск новых потоков для левой и правой частей
+    // Один из них будет от текущего левого края до середины,
+    // а другой от середины+1 до правого края
+    pthread_t left_thread, right_thread;
+    
+    // Выделение памяти для результата дочернего левого потока
+    long result_left_size = mid_idx - data->left_idx + 1;
+    long* result_left = (long*)(malloc(sizeof(long) * result_left_size));
+
+    // Сбор остальных данных в структуру
+    s_data data_left = { .iter = data->iter + 1,
+                         .input = data->input,
+                         .ar = data->ar,
+                         .left_idx = data->left_idx,
+                         .right_idx = mid_idx,
+                         .result = result_left};
+
+    // Аналогично для правой части
+    long result_right_size = data->right_idx - mid_idx + 1;
+    long* result_right = (long*)(malloc(sizeof(long) * result_right_size));
+    s_data data_right = { .iter = data->iter + 1,
+                         .input = data->input,
+                         .ar = data->ar,
+                         .left_idx = mid_idx + 1,
+                         .right_idx = data->right_idx,
+                         .result = result_right};
+
+    // Запуск и ожидание завершения
+    pthread_create(&left_thread, NULL, merge_sort_thread, &data_left);
+    pthread_create(&right_thread, NULL, merge_sort_thread, &data_right);
+    pthread_join(left_thread, NULL);
+    pthread_join(right_thread, NULL);
+
+    // Соединение результатов дочерних потоков
+    merge_arrays(data, result_left, result_left_size, result_right, result_right_size); 
+
+    // Освобождение результатов дочерних потоков из памяти
+    free(result_left);
+    free(result_right);
 
     return NULL;
 }
